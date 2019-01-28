106525d34d6faf7e42c4cc9b7e53e54d3fd85e77
Wille Tuovinen <wille.tuovinen@metropolia.fi>
2019-01-28 10:27:38 +0200

    gfd
    jjh

--- week1/01 JavaScript Tools/guess.js
+++ week1/01 JavaScript Tools/guess.js
@@ -5,7 +5,7 @@ var counter;
 // I HAD A BIT OF UNSOLVED PROBLEMS HERE. I COULDNT GET "VAR USERNAME" WORK GLOBALLY.
 // THATS WHY THIS IS A BIT OF MESS...
 
-function init() {
+function init(UserNumber) {
     var UserNumber = document.getElementById("number").value;
     //This function sets up an event listener on the form.
     console.log("guess number between 1-10");
@@ -27,7 +27,7 @@ function showWin(UserNumber) {
     console.log("Your guess " + UserNumber +" was correct! You won the game! Press F5 to start over");
     console.log(UserNumber + " was a right answer");
 }
-function showError(UserNumber, counter) {
+function showError(UserNumber) {
     //function to show a message on the console telling the player their guess is incorrect.
     counter += 1;
     if (counter == 5) {    // didn't get this working
@@ -38,10 +38,8 @@ function showError(UserNumber, counter) {
         console.log("Your guess " + UserNumber + " was wrong!!!. guess again!");
         console.log("Right anser was " + targetNumber);                         // JUST FOR TESTING IT WORKS
         console.log("you have " , 5 - counter , " attempts to go.");
-
     }
 }
-
 function showLoss() {
     //function to show a message on the console telling the player they lose.
     console.log("Your guess was AGAIN incorrect! GAME OVER! Press F5 to start again.");
