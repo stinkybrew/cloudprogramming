53b9c519bff65aa5bc7b95023d15bfa9d1821fe3
Wille Tuovinen <wille.tuovinen@metropolia.fi>
2019-01-28 22:54:26 +0200

    w1 01 todo2 works
    w1 01 todo2 works

--- week1/01 JavaScript Tools/guess.js
+++ week1/01 JavaScript Tools/guess.js
@@ -1,6 +1,4 @@
 
-let targetNumber = Math.floor(Math.random() * 10) + 1;
-var UserNumber = document.getElementById("number").value;
 var counter;
 
 // I HAD A BIT OF UNSOLVED PROBLEMS HERE. I COULDNT GET "VAR USERNAME" WORK GLOBALLY.
@@ -8,40 +6,43 @@ var counter;
 
 function init() {
     //This function sets up an event listener on the form.
+    let targetNumber = Math.floor(Math.random() * 10) + 1;
+    var UserNumber = document.getElementById("number").value;
     console.log("guess number between 1-10");
     check();
-}
-function check(UserNumber) {
-    var UserNumber = document.getElementById("number").value;
-    //functio value from the event listener and check it against the `targetNumber`.
-    if (targetNumber == UserNumber){
-        showWin(); // TOIMII
-    }
-    else {
-        showError(); //TOIMII
-    }
-}
-function showWin(UserNumber) {
-    var UserNumber = document.getElementById("number").value;
-    //function to  show a message on the console telling the player they win.
-    console.log("Your guess " + UserNumber +" was correct! You won the game! Press F5 to start over");
-    console.log(UserNumber + " was a right answer");
-}
-function showError(UserNumber) {
-    var UserNumber = document.getElementById("number").value;
-    //function to show a message on the console telling the player their guess is incorrect.
-    counter += 1;
-    if (counter == 5) {    // didn't get this working
-        showLoss();
-    }
-    else {
-        console.log("Your guess " + UserNumber + " was wrong!!!. guess again!");
-        console.log("Right anser was " + targetNumber);                         // JUST FOR TESTING IT WORKS
-        console.log("you have " , 5 - counter , " attempts to go.");
-    }
-}
-function showLoss() {
-    //function to show a message on the console telling the player they lose.
-    console.log("Your guess was AGAIN incorrect! GAME OVER! Press F5 to start again.");
 
+    function check(UserNumber) {
+        var UserNumber = document.getElementById("number").value;
+        //functio value from the event listener and check it against the `targetNumber`.
+        if (targetNumber == UserNumber){
+            showWin(); // TOIMII
+        }
+        else {
+            showError(); //TOIMII
+        }
+    }
+    function showWin(UserNumber) {
+        var UserNumber = document.getElementById("number").value;
+        //function to  show a message on the console telling the player they win.
+        console.log("Your guess " + UserNumber +" was correct! You won the game! Press F5 to start over");
+        console.log(UserNumber + " was a right answer");
+    }
+    function showError(UserNumber) {
+        var UserNumber = document.getElementById("number").value;
+        //function to show a message on the console telling the player their guess is incorrect.
+        counter += 1;
+        if (counter == 5) {    // didn't get this working
+            showLoss();
+        }
+        else {
+            console.log("Your guess " + UserNumber + " was wrong!!!. guess again!");
+            console.log("Right anser was " + targetNumber);                         // JUST FOR TESTING IT WORKS
+            console.log("you have " , 5 - counter , " attempts to go.");
+        }
+    }
+    function showLoss() {
+        //function to show a message on the console telling the player they lose.
+        console.log("Your guess was AGAIN incorrect! GAME OVER! Press F5 to start again.");
+
+    }
 }
