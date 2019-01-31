b9ecf959a5a5a7d81e1f99e629edf98be027051d
stinkybrew <wille.tuovinen@metropolia.fi>
2019-01-31 21:21:59 +0200

    ddd
    ddd

--- muistiinpanot/new For-loop in ACMAscript6.js
+++ muistiinpanot/new For-loop in ACMAscript6.js
@@ -2,10 +2,36 @@
 
 let arr = [1, 2, 3];
 for (let k = 0; k < arr.length; k++) {
-  console.log(arr[k]);
+    console.log(arr[k]);
 }
 
 let obj = {a: 1, b: 2, c: 3};
 for (let v in obj) {
-  console.log(v);
+    console.log(v);
 }
+
+
+
+
+// arrayn voi toteuttaa seuraavalla tavalla
+var arr = [2, 3, 7, 8];
+
+arr.forEach(function(el) {
+    console.log(el * 2);
+});
+
+// and
+const arr = [2, 3, 7, 8];
+
+arr.forEach(v => {
+    console.log(v * 2);
+});
+
+//JOKU HYVIN SEKAVA ARRAY juttu :O
+const printOdds = (arr) => {
+    arr.forEach(el => {
+        if (el % 2 != 0) console.log(el);
+    });
+}
+
+
