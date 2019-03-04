372961fc4e0847b56bf01a2514958b9ec519d288
stinkybrew <wille.tuovinen@metropolia.fi>
2019-03-04 18:16:37 +0200

    ...
    ...

--- week3/02/TODO4/js/slot.js
+++ week3/02/TODO4/js/slot.js
@@ -1,6 +1,6 @@
 Vue.component('my-list', {
   template: '#my-list',
-  props: [ 'title', 'items' ]
+  props: [ 'title', 'items', 'items2' ]
 });
 
 new Vue({
@@ -10,6 +10,11 @@ new Vue({
       { name: 'Michelle', tweet: '@MichelleObama' }, 
       { name: 'Barack', tweet: '@BarackObama' }, 
       { name: 'Donald', tweet: '@realDonaldTrump' }
+    ],
+    parties: [
+      { name: 'Republican Party', senators: 53, },
+      { name: 'Democratic Party', senators: 45 },
+      { name: 'Independent', senators: 2 }
     ]
   }
 });
