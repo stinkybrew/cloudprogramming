a0d8954ab8928de40b28e0fb9d33f0b3c3b54df2
Wille Tuovinen <wille.tuovinen@metropolia.fi>
2019-03-05 12:11:47 +0200

    ...
    ...

--- week3/02/TODO4/js/slot.js
+++ week3/02/TODO4/js/slot.js
@@ -15,34 +15,32 @@ new Vue({
       { namez: 'Republican Party', senators: 53, },
       { namez: 'Democratic Party', senators: 45 },
       { namez: 'Independent', senators: 2 }
-    ],
-    created: function () {
-      let partylist = this.parties
-      const myObjStr = JSON.stringify(partylist)
-      //const myobjc2 = JSON.parse(myObjStr)
-      return this.myObjStr
-    },
-    created: function mergeRecursive(obj1, obj2) {
-      for (var p in obj2) {
-        try {
-          // Property in destination object set; update its value.
-          if (obj2[p].constructor == Object) {
-            obj1[p] = this.mergeRecursive(obj1[p], obj2[p]);
-    
-          } else {
-            obj1[p] = obj2[p];
-    
-          }
-    
-        } catch (e) {
+    ]
+  },
+  created: function () {
+    let partylist = this.parties
+    const myObjStr = JSON.stringify(partylist)
+    //const myobjc2 = JSON.parse(myObjStr)
+    return this.myObjStr
+  },
+  created: function merge(obj1, obj2) {
+    for (var p in obj2) {
+      try {
+      // Property in destination object set; update its value.
+      if (obj2[p].constructor == Object) {
+          obj1[p] = this.merge(obj1[p], obj2[p]);
+        } else {
           obj1[p] = obj2[p];
-    
         }
+      } catch (e) {
+        obj1[p] = obj2[p];
       }
-      return obj1;
-    },
-    created: function () {
+    }
+    return this.obj1;
+  },
+  created: function () {
+    merge(this.persons,this.parties)
+    console.log( JSON.stringify(this.persons) )
 
     }
-  }
 });
