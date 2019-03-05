fddac490e44e4cb51bc593dab71bbd7f7e5332b0
Wille Tuovinen <wille.tuovinen@metropolia.fi>
2019-03-05 13:55:17 +0200

    ...
    ...

--- week3/02/TODO4/js/slot.js
+++ week3/02/TODO4/js/slot.js
@@ -18,16 +18,24 @@ new Vue({
     ]
   },
   created: function () {
-    let partylist = this.parties
-    const myObjStr = JSON.stringify(partylist)
+    vm = this
+    let personlist = JSON.stringify(vm.persons)
+    let partylist = JSON.stringify(vm.parties)
     //const myobjc2 = JSON.parse(myObjStr)
-    return myObjStr
+    console.log( this.persons );
+    return partylist, personlist 
   },
-  created: function merge(obj1, obj2) {
-    for (var p in obj2) {
+  created: function mergeRecursive(obj1, obj2) {
+    if ( ! obj1 || obj2) {
+      return;
+    }
+    a(--x);
+    vm = this
+    // ----------------------------------------------
+   /* for (var p in obj2) {
       try {
       // Property in destination object set; update its value.
-      if (obj2[p].constructor == Object) {
+      if (obj2[p].index == Object) {
           obj1[p] = this.merge(obj1[p], obj2[p]);
         } else {
           obj1[p] = obj2[p];
@@ -35,9 +43,36 @@ new Vue({
       } catch (e) {
         obj1[p] = obj2[p];
       }
+
+    } */
+
+    // ----------------------------------------------
+
+    for (var i in obj2) {
+      try {
+        // Property in destination object set; update its value.
+        if (obj2[i].constructor == Object) {
+          obj1[i] = this.mergeRecursive(obj1[i], obj2[i]);
+        } else {
+          obj1[i] = obj2[i];
+        }
+      } catch (e) {
+        obj1[i] = obj2[i];
+      }
     }
-    merge(this.persons,this.parties)
-    console.log( JSON.stringify(this.persons) )
-    return this.obj1;
+    return obj1;
   },
+  created: function () {
+    mergeRecursive(this.persons,this.parties)
+    console.log(JSON.stringify(this.persons))
+    
+  }
+
+    // -----------------------------------------------  
+   /* mergeRecursive(this.personlist,this.partylist)
+    const myobjc2 = JSON.parse(this.personlist)
+    vm.persons = myobjc2
+    console.log( this.partylist )
+    return this.obj1*/
+  
 });
