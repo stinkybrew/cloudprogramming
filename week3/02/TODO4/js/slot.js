7e25de4a80a3b2a3468ec8b43e17fede07d106c8
Wille Tuovinen <wille.tuovinen@metropolia.fi>
2019-03-05 12:01:26 +0200

    first try to merge lists
    first try to merge lists

--- week3/02/TODO4/js/slot.js
+++ week3/02/TODO4/js/slot.js
@@ -19,8 +19,30 @@ new Vue({
     created: function () {
       let partylist = this.parties
       const myObjStr = JSON.stringify(partylist)
-      const myobjc2 = JSON.parse(myObjStr)
+      //const myobjc2 = JSON.parse(myObjStr)
       return this.myObjStr
+    },
+    created: function mergeRecursive(obj1, obj2) {
+      for (var p in obj2) {
+        try {
+          // Property in destination object set; update its value.
+          if (obj2[p].constructor == Object) {
+            obj1[p] = this.mergeRecursive(obj1[p], obj2[p]);
+    
+          } else {
+            obj1[p] = obj2[p];
+    
+          }
+    
+        } catch (e) {
+          obj1[p] = obj2[p];
+    
+        }
+      }
+      return obj1;
+    },
+    created: function () {
+
     }
   }
 });
