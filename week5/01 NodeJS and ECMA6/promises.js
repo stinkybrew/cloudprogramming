dc5a6be9129f7dbcaa3906b4e2278f9767fe224c
stinkybrew <wille.tuovinen@metropolia.fi>
2019-03-21 18:30:02 +0200

    ...
    ...

--- week5/01 NodeJS and ECMA6/promises.js
+++ week5/01 NodeJS and ECMA6/promises.js
@@ -6,7 +6,7 @@ const stdin = process.openStdin()
 
 /* in this function the anonymous function inside the promise has been declared using the 'traditional' syntax. */
 function reverse(string) {
-    return new Promise(function(resolve, reject) {
+    return new Promise((resolve, reject) => {
         /* notice the use of the 'const' keyword instead of the usual 'var'. 
         This indicates a constant or 'immutable variable'. */
         const reversed = string.split('').reverse().join('')
@@ -18,11 +18,19 @@ function reverse(string) {
 /* In this function the anonymous function is declared using the new ECMA6 'arrow' syntax. */
 function capitalise(string) {
     return new Promise((resolve, reject) => {
-        /* the 'let' keyword means the scope of the following variable is limited to the current code block rather than being scoped by the function. 
+        /* the 'let' keyword means the scope of the following variable is limited to the current code block rather 
+        than being scoped by the function. 
         Use of this required strict mode to be implemented. */
         let cap = string.charAt(0).toUpperCase() + string.slice(1)
         //reject('could not capitalise the string')
-        resolve(cap)
+        reject(cap)
+    })
+}
+
+function caplast(string) {
+    return new Promise((resolve, reject) => {
+        let caplast = string.charAt(-1).toUpperCase() + string.slice(1)
+        resolve(caplast)
     })
 }
 
@@ -37,9 +45,14 @@ function promise1(data) {
   reverse(data).then(function(data) {
     /* capitalise() returns a new promise. */
     return capitalise(data)
-  }).then(function(data) {
+  })
+  capitalise(data).then(function(data) {
+    return caplast(data)
+  })
+  .then(function(data) {
     console.log(data)
-  }).catch(function(err) {
+  })
+  .catch(function(err) {
     console.log('an error occurred: '+err)
   })
 }
@@ -56,7 +69,8 @@ function promise2(data) {
   })
 }
 
-/* In this third version we take advantage of the fact that when resolving a promise the return value is implicit so we don't need to include it. */
+/* In this third version we take advantage of the fact that when resolving a promise the return value is implicit so 
+we don't need to include it. */
 function promise3(data) {
   reverse(data)
     .then( data => capitalise(data)  )
@@ -65,7 +79,8 @@ function promise3(data) {
 }
 
 /* In this fourth version we eliminate the parameter to the second function. 
-This only works if the second function takes a single parameter and its value is the returned object from the previous function. */
+This only works if the second function takes a single parameter and its value is the returned object from the previous 
+function. */
 function promise4(data) {
   reverse(data)
     .then(capitalise)
